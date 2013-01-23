758c0d1b3a640885285b827533def70e38e83956
henry14 <henrytebs@gmail.com>
2013-01-23 17:14:11 +0300

    Second Commit

--- 2UG/2UG/MainPage.xaml.cs
+++ 2UG/2UG/MainPage.xaml.cs
@@ -25,11 +25,6 @@ namespace _2UG
             
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void button1_Click_1(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(new Uri("/Transport.xaml", UriKind.Relative));
@@ -37,12 +32,42 @@ namespace _2UG
 
         private void MenuItem1_Click(object sender, EventArgs e)
         {
-            NavigationService.Navigate(new Uri("/", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/About.xaml", UriKind.Relative));
         }
 
         private void MenuItem2_Click(object sender, EventArgs e)
         {
-            NavigationService.Navigate(new Uri("/", UriKind.Relative));
+            NavigationService.Navigate(new Uri("/History.xaml", UriKind.Relative));
+        }
+
+        private void image4_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void hotel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Hotel.xaml", UriKind.Relative));
+        }
+
+        private void transport_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Transport.xaml", UriKind.Relative));
+        }
+
+        private void attraction_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Attraction.xaml", UriKind.Relative));
+        }
+
+        private void spot_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Spot.xaml", UriKind.Relative));
+        }
+
+        private void convertor_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Convertor.xaml", UriKind.Relative));
         }
     }
 }
