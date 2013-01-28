25222ffb4c64ad09c7958f589515ef154d67cfa6
jmpango <mpiksjonathan@gmail.com>
2013-01-28 12:15:58 +0300

    Worked on the special hire form

---
+++ 2UG/2UG/Model/SpecialHire.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+// Represent a special hire object
+
+namespace _2UG.Model
+{
+    public class SpecialHire
+    {
+        private String name;
+        private String address;
+        private String telphone;
+        private String district;
+        private String iconUri;
+
+        public String Name
+        {
+            get { return name;}
+            set { name = value; }
+        }
+
+        public String Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public String Telphone
+        {
+            get { return telphone; }
+            set { telphone = value; }
+        }
+
+        public String District
+        {
+            get { return district; }
+            set { district = value; }
+        }
+
+        public String IconUri
+        {
+            get { return iconUri; }
+            set { iconUri = value; }
+        }
+    }
+}
--- 2UG/2UG/PivotPage1.xaml.cs
+++ 2UG/2UG/PivotPage1.xaml.cs
@@ -10,14 +10,75 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using _2UG.Model;
+using System.Xml.Linq;
 
 namespace _2UG
 {
     public partial class PivotPage1 : PhoneApplicationPage
     {
+        private static XDocument loadSpecialHireXMl = XDocument.Load("database/specialHire.xml");
+
         public PivotPage1()
         {
             InitializeComponent();
+            populateSpecialHireListBox();
+        }
+
+        private void populateSpecialHireListBox(){
+            specialHireList.ItemsSource = retrieveSpecialHireData("");
+        }
+
+        private IEnumerable<SpecialHire> retrieveSpecialHireData(string districtName)
+        {
+            IEnumerable<SpecialHire> specialHireData = null;
+            if (districtName.Equals(""))
+            {
+                specialHireData = from sHire in loadSpecialHireXMl.Descendants("special_hire")
+                                  select new SpecialHire()
+                                  {
+                                      Name = (String)sHire.Element("name"),
+                                      Address = (String)sHire.Element("address"),
+                                      Telphone = (String)sHire.Element("telphone"),
+                                      District = convertFirstElementToUpperCase((String)sHire.Element("district")),
+                                      IconUri = (String)sHire.Element("icon")
+                                  };
+            }
+            else {
+                specialHireData = from sHire in loadSpecialHireXMl.Descendants("special_hire")
+                                  where sHire.Element("district").Value.Contains(districtName.ToLower())
+                                  select new SpecialHire()
+                                  {
+                                      Name = (String)sHire.Element("name"),
+                                      Address = (String)sHire.Element("address"),
+                                      Telphone = (String)sHire.Element("telphone"),
+                                      District = convertFirstElementToUpperCase((String)sHire.Element("district")),
+                                      IconUri = (String)sHire.Element("icon")
+                                  };
+            }
+            return specialHireData;
+        }
+
+         private string convertFirstElementToUpperCase(string text)
+        {
+
+            return char.ToUpper(text[0]) + text.Substring(1);
+        }
+
+        private void specialHire_Searchbox_handler(Object sender, KeyEventArgs e)
+        {
+            var specialHireData = retrieveSpecialHireData(specialHireSearchBox.Text);
+            if(specialHireData.Any() == false){
+                if (e.Key == Key.Enter)
+                {
+                    SpecialHire spHire = new SpecialHire();
+                    spHire.Name = "No special Hire found!";
+
+                    specialHireData = new[] { spHire };
+                }
+            }
+
+            specialHireList.ItemsSource = specialHireData;
         }
     }
 }
